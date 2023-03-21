b6493797457f6151055e98b41a611cdc54014137
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-03-21 22:39:38 +0200

    add fontFamily to ourApp

--- lib/style.dart
+++ lib/style.dart
@@ -3,6 +3,6 @@ import 'package:flutter/cupertino.dart';
 abstract class Styles {
   static const titleMedium = TextStyle(
     fontWeight: FontWeight.w600,
-    fontSize: 20,
+    fontSize: 18,
   );
 }
