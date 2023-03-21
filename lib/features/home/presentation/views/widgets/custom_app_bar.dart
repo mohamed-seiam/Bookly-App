ba0f4f3f8ac10009044960d4d81a2ef73d06b020
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-03-21 02:19:53 +0200

    create homeView body and add custom AppBar

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28.0,vertical: 44),
+      child: Row(
+        children:  [
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
