54a64b07c648649f2cf2c96cb7331d3fe9c7617c
mohamed-seiam <113236919+mohamed-seiam@users.noreply.github.com>
2023-03-22 22:52:08 +0200

    create bookDetailsView

---
+++ lib/features/home/presentation/views/widgets/book_details_body_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsBodyView extends StatelessWidget {
+  const BookDetailsBodyView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container();
+  }
+}
